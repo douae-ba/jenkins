f21343214323632adfb0b738f89a38720ee8f61d
Douae Benachour <douaebenachour@gmail.com>
2026-06-01 09:50:08 +0200

    third commit

--- DeRien.java
+++ DeRien.java
@@ -1,5 +1,5 @@
 
-public class HelloWorld {
+public class DeRien {
 
     public static void main(String[] args) {
         System.out.println("hello depuis classe DeRien");
--- Merci.java
+++ Merci.java
@@ -1,5 +1,5 @@
 
-public class HelloWorld {
+public class Merci {
 
     public static void main(String[] args) {
         System.out.println("hello depuis classe Merci");
