6df69ec6712c2ecd7e508a062a929079a16dc22e
Douae Benachour <douaebenachour@gmail.com>
2026-06-01 10:02:42 +0200

    commit 4

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello depuis classe HelloWorld");
+        System.out.println("hello depuis classe HelloWorld version 2g");
     }
 
 }
